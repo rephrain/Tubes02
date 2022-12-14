25b0d4068c32ba749efdfaa4e2f98ddd56ed7de9
rephrain <rephrain1@gmail.com>
2022-12-14 10:20:53 +0700

    [ADD] binding

--- app/src/main/java/com/example/tubes2/MainActivity.java
+++ app/src/main/java/com/example/tubes2/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.tubes2.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
