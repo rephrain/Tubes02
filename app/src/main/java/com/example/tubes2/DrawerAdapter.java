6fdd7cd48a6e6d319232cf1fbcf248a0d437e0e6
rephrain <rephrain1@gmail.com>
2023-01-07 02:19:25 +0700

    [UPDATE] home layout

--- app/src/main/java/com/example/tubes2/DrawerAdapter.java
+++ app/src/main/java/com/example/tubes2/DrawerAdapter.java
@@ -4,11 +4,16 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
@@ -43,6 +48,19 @@ public class DrawerAdapter extends RecyclerView.Adapter<DrawerAdapter.ViewHolder
                     case 2:
                         break;
                     case 3:
+                        AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                        LoginFragment login = LoginFragment.newInstance("Fragment Login");
+                        HomeFragment home = HomeFragment.newInstance("Fragment Home");
+                        FragmentTransaction ft = activity.getSupportFragmentManager().beginTransaction();
+                        if (login.isAdded()){
+                            ft.show(login);
+                        } else{
+                            ft.replace(R.id.fragment_container,login).addToBackStack(null);
+                        }
+                        if (home.isAdded()){
+                            ft.remove(home);
+                        }
+                        ft.commit();
                         break;
                 }
             }
