d06d54b877f811800cb4099737d5c813955e3c20
dittoryanda <6182001056@student.unpar.ac.id>
2023-01-10 13:49:06 +0700

    tambah model student dosen, task

---
+++ app/src/main/java/com/example/tubes2/model/Lecturer.java
@@ -0,0 +1,7 @@
+package com.example.tubes2.model;
+
+public class Lecturer extends User{
+    public Lecturer(String email, String password, String role) {
+        super(email, password, role);
+    }
+}
